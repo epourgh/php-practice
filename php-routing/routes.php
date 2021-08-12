774ddb785c3387023194ac3e20700470689b6a06
epourgh <epourgh@gmail.com>
2021-08-11 23:59:48 -0500

    migrate routing to own file

--- php-routing/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
-
-Route::get('/');
-
-Route::get('/user/:id');
-
-Route::get('/user/:id?name=:name&password=:password&email=:email');
